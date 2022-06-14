2a0f718124f8009a5f1ed28a3dba6cf8f9743e2e
z-najafiyan <213.najafiyan@gmail.com>
2022-06-13 21:55:22 -0700

    upload employment

---
+++ employment/urls.py
@@ -0,0 +1,61 @@
+"""job URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.template.defaulttags import url
+from django.urls import path, re_path, include
+from rest_framework import routers, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from candidate.views import CandidateView
+from common.views import CommonView
+from employer.views import EmployerView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from employment import settings
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ESTATE API",
+        default_version='v1',
+        description="Test description",
+        # terms_of_service="http://127.0.0.1:8001",
+        # contact=openapi.Contact(email="contact@snippets.local"),
+        # license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+router = routers.DefaultRouter()
+router.register(r"candidate", viewset=CandidateView, basename="candidate")
+router.register(r"common", viewset=CommonView, basename="common")
+router.register(r"employer", viewset=EmployerView, basename="employer")
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include(router.urls)),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-auth/', include('rest_framework.urls')),
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
