4d5a99336bcb18b34ac0351cc8cc995fe0f82adf
z-najafiyan <213.najafiyan@gmail.com>
2022-08-04 14:14:48 -0700

    allow any

--- common/views.py
+++ common/views.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from django.shortcuts import render
 # Create your views here.
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
