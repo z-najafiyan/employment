6ba5aa3048bec1d253085f1204e5127f0a587389
z-najafiyan <213.najafiyan@gmail.com>
2022-08-12 14:44:46 -0700

    update city

--- common/views.py
+++ common/views.py
@@ -13,30 +13,30 @@ from other_files.response_serialzer import ProvinceResponseSerializer, CityRespo
 
 class CommonView(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
+
     @action(methods=["post"], detail=False)
     def create_data(self, request):
         data = pd.read_excel(r'other_files/province_list.xlsx')
-        data_obj = pd.DataFrame(data,).values
-        for city_name,province_name in data_obj:
-            obj_province,_=Province.objects.get_or_create(name=province_name)
-            obj_province,_=City.objects.get_or_create(name=city_name,province=obj_province)
-        return Response({"message":"done"},status=status.HTTP_200_OK)
-
-
-    @action(methods=["get"], detail=False)
-    def province(self,request):
-        instances=Province.objects.all()
-        serializer=ProvinceResponseSerializer(instances,many=True)
-        return Response(serializer.data,)
+        data_obj = pd.DataFrame(data, ).values
+        for city_name, province_name in data_obj:
+            obj_province, _ = Province.objects.get_or_create(name=province_name)
+            obj_province, _ = City.objects.get_or_create(name=city_name, province=obj_province)
+        return Response({"message": "done"}, status=status.HTTP_200_OK)
 
     @action(methods=["get"], detail=False)
-    def city(self,request):
-        instances=City.objects.all()
-        serializer=CityResponseSerializer(instances)
-        return Response(serializer.data,)
+    def province(self, request):
+        instances = Province.objects.all()
+        serializer = ProvinceResponseSerializer(instances, many=True)
+        return Response(serializer.data, )
 
     @action(methods=["get"], detail=False)
-    def category(self,request):
-        instances=Category.objects.all()
-        serializer=CategoryResponseSerializer(instances,many=True)
-        return Response(serializer.data,)
+    def city(self, request):
+        instances = City.objects.all()
+        serializer = CityResponseSerializer(instances,many=True)
+        return Response(serializer.data, )
+
+    @action(methods=["get"], detail=False)
+    def category(self, request):
+        instances = Category.objects.all()
+        serializer = CategoryResponseSerializer(instances, many=True)
+        return Response(serializer.data, )
