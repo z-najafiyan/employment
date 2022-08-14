2a32c4b860cd4a73889fb914fd5ed9f80b3f163a
z-najafiyan <213.najafiyan@gmail.com>
2022-08-13 23:46:35 -0700

    update paginations

--- common/paginations.py
+++ common/paginations.py
@@ -4,9 +4,9 @@ from rest_framework.response import Response
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 8
+    page_size = 100
     page_size_query_param = 'page_size'
-    max_page_size = 8
+    max_page_size = 100
 
     def get_paginated_response(self, data):
         return Response(
